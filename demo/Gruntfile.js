8491a7c5e683ff4c91db37a2e1367426c1df1e19
Rob Taylor <roboncode@gmail.com>
2014-07-14 19:57:02 -0600

    Created example and gruntfile for second example

--- demo/Gruntfile.js
+++ demo/Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
 
     var tasks = [
-        'jshint',
         'uglify'
     ];
     
@@ -11,19 +10,6 @@ module.exports = function (grunt) {
             '* <%= pkg.name %> v.<%= pkg.version %>\n' +
             '* Rob Taylor. MIT ' + new Date().getFullYear() + '\n' +
             '*/\n',
-//        wrapStart: '(function(){\n',
-//        wrapEnd: '\n}());\n',
-        jshint: {
-            // define the files to lint
-            files: ['js/**/*.js'],
-            // configure JSHint (documented at http://www.jshint.com/docs/)
-            options: {
-                // more options here if you want to override JSHint defaults
-                globals: {
-                    loopfunc: false
-                }
-            }
-        },
         uglify: {
             build: {
                 options: {
@@ -32,33 +18,23 @@ module.exports = function (grunt) {
                     preserveComments: 'some',
                     beautify: true,
                     exportAll: true,
-                    wrap: '<%= pkg.packageName %>',
+//                    wrap: '<%= pkg.packageName %>',
                     banner: '<%= banner %>'
                 },
                 files: {
                     './build/<%= pkg.filename %>.js': [
-                        'src/prototype/*.js',
-                        'src/**/_namespace.js',
-                        'src/utils/*.js',
-                        'src/validators/*.js',
-                        'src/core/*.js',
                         'src/**/*.js'
                     ]
                 }
             },
             build_min: {
                 options: {
-                    wrap: '<%= pkg.packageName %>',
+//                    wrap: '<%= pkg.packageName %>',
                     banner: '<%= banner %>',
                     exportAll: true
                 },
                 files: {
                     './build/<%= pkg.filename %>.min.js': [
-                        'src/prototype/*.js',
-                        'src/**/_namespace.js',
-                        'src/utils/*.js',
-                        'src/validators/*.js',
-                        'src/core/*.js',
                         'src/**/*.js'
                     ]
                 }
@@ -66,8 +42,6 @@ module.exports = function (grunt) {
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', tasks);
