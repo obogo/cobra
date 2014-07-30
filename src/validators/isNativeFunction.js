04603722bd06c20f704b8a2d452eeb3fe090d03d
Rob Taylor <roboncode@gmail.com>
2014-07-30 10:51:57 -0600

    updated isNativeFunction to include functions residing on the global namespace

--- src/validators/isNativeFunction.js
+++ src/validators/isNativeFunction.js
@@ -1,6 +1,12 @@
 /* global validators */
+/**
+ * This make the assumption that functions are not being used that have been placed on the global namespace.
+ * Had to do this because this was failing in PhantomJS
+ * @param value
+ * @returns {boolean}
+ */
 validators.isNativeFunction = function (value) {
     return typeof value === 'function' &&
         (value.toString().indexOf('[native code]') > -1 ||
-            value.toString().indexOf('[function]') > -1);
+            value.toString().indexOf('[function]') > -1) || !!window[value.name];
 };
